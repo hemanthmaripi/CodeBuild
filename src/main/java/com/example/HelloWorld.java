a6fb82f074a6a2cd044a476ae1ac6415686cea8c
Hemanth Maripi <141854315+hemanthmaripi@users.noreply.github.com>
2025-11-14 11:30:37 +0530

    Update greeting message in HelloWorld class

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello from AWS CodeBuild! I'm Hemanth");
+        System.out.println("Hello from AWS CodeBuild! I'm Hemanth, How's your day?");
     }
 }
 
